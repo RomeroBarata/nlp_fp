bd127bb415a8722fd54118464afcde52ff60d70d
RomeroBarata <romero.barata@gmail.com>
2015-12-12 02:41:40 +1100

    Main structure of the program

---
+++ main.py
@@ -0,0 +1,14 @@
+import nltk
+import pprint
+
+## Constants ##
+
+## Collect the data ##
+
+## Split the data into training and testing ##
+
+## Train a classifier ##
+
+## Classify unseen tweets ##
+
+## Assess the results ##
